8c123d942e51ebb31f7c325100c7b99ac591fa54
rakeeb-hossain <rakeeb.hossain1@gmail.com>
2024-12-24 22:02:18 -0800

    basic xtts working

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import gcp_tts
 import tortoise_tts
+import xtts_v2
 
 def main():
     # gcp_tts.tts("out/output.mp3", "Hello, World!")
-    tortoise_tts.tts("Hello, World!", "out/output.mp3")
+    # tortoise_tts.tts("Hello, World!", "out/output.mp3")
+    xtts_v2.tts("Hello, World!", "out/output.mp3")
 
 if __name__ == "__main__":
     main()
--- xtts_v2.py
+++ xtts_v2.py
@@ -1,2 +1,26 @@
+import torch
+import sys
+from TTS.api import TTS
+from TTS.tts.configs.xtts_config import XttsConfig, XttsAudioConfig, XttsArgs
+from TTS.config import BaseDatasetConfig
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+torch.serialization.add_safe_globals([XttsConfig, XttsAudioConfig, BaseDatasetConfig, XttsArgs])
+
 def tts(text: str, out_path: str):
-    pass
+    # Initialize TTS with XTTS v2 model
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+    print(tts.is_multi_speaker)
+    print("TTS model loaded")
+    print("Speakers: ", tts.synthesizer.tts_model.speaker_manager.name_to_id)
+
+    # Generate speech
+    tts.tts_to_file(
+        text=text,
+        file_path=out_path,
+        speaker="Claribel Dervla",
+        speaker_wav=None,  # Can specify speaker reference audio
+        language="en"
+    )
+    print(f"Audio content written to file '{out_path}'")
